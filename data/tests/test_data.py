3af4fa9d357b4028217f10b0947808c3c633fe3a
Anna Liu <liuannaceo@gmail.com>
2015-11-09 11:35:10 -0800

    modify test_data.py

--- data/tests/test_data.py
+++ data/tests/test_data.py
@@ -6,13 +6,11 @@ from .. import data
 
 
 def test_check_hashes():
-    tf = tempfile.NamedTemporaryFile(delete=False)
-    fname = tf.name
     with tempfile.NamedTemporaryFile() as temp:
         temp.write(b'Some data')
         temp.flush()
         fname = temp.name
-        d = {fname: "5b82f8bf4df2bfb0e66ccaa7306fd024"}
+        d = {fname: data.generate_file_md5(fname)}
         assert data.check_hashes(d)
         d = {fname: "4b82f8bf4df2bfb0e66ccaa7306fd024"}
         assert not data.check_hashes(d)
