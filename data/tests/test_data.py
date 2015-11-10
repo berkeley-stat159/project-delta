ad74169b6f3c10ab6437eb45a5d6d07b9ac2df6a
Victor Kong <victorkong94@gmail.com>
2015-11-10 01:41:56 -0800

    Add test for make_hash_dict.py

--- data/tests/test_data.py
+++ data/tests/test_data.py
@@ -16,3 +16,10 @@ def test_check_hashes():
         assert data.check_hashes(d)
         d = {fname: "4b82f8bf4df2bfb0e66ccaa7306fd024"}
         assert not data.check_hashes(d)
+
+def test_make_hash_dict():
+    l = "http://www.jarrodmillman.com/rcsds/_downloads/ds107_sub001_highres.nii"
+    urllib.request.urlretrieve(l, filename = "ds107_sub001_highres.nii")
+    hash_O = make_hash_dict.make_hash_dict(".")["./ds107_sub001_highres.nii"]
+    hash_E = "fd733636ae8abe8f0ffbfadedd23896c"
+    assert hash_O == hash_E
