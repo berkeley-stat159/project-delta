f29921bfb3b7d6a3a501f240f0b283d06f7b896d
Anna Liu <liuannaceo@gmail.com>
2015-11-16 21:29:35 -0800

    fix coverage problems

--- Makefile
+++ Makefile
@@ -6,7 +6,10 @@ clean:
 	find . -name "*.so" -o -name "*.pyc" -o -name "*.pyx.md5" | xargs rm -f
 
 coverage:
-	nosetests code/utils data --with-coverage --cover-package=data  --cover-package=utils
+	wget https://www.dropbox.com/s/qrf2jqgr1pyr7bi/behavdata.txt
+	nosetests data --with-coverage --cover-package=data
+	nosetests code/utils --with-coverage --cover-package=utils
+	rm -f behavdata.txt
 
 test:
 	nosetests code/utils data
--- code/utils/tests/test_utils.py
+++ code/utils/tests/test_utils.py
@@ -2,23 +2,21 @@
 Testing functions in utils_functions.py
 
 Run with:
-	nosetests test_utils_functions.py
+	nosetests test_utils.py
 
 """
 from __future__ import division, print_function, absolute_import
 import numpy as np
-import sys
-sys.path.append("../.")
-from utils import *
+from .. import utils_functions
 
-filename = "../.././behavdata.txt"
+filename = "behavdata.txt"
 def test_read_txt_files():
-	arr = read_txt_files(filename)
+	arr = utils_functions.read_txt_files(filename)
 	assert isinstance(arr, np.ndarray)
 	return True
 
 def test_construct_mat():
-	arr = read_txt_files(filename)
-	design_mat = construct_mat(arr)
+	arr = utils_functions.read_txt_files(filename)
+	design_mat = utils_functions.construct_mat(arr)
 	assert len(design_mat.shape)==2 and design_mat.shape[0] <= arr.shape[0]
 	return True
