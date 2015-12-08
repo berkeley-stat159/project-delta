165a309d1af499ad8f7b94a9a1ea921718e88c5d
Victor Kong <VictorKong@airbears2-10-142-132-177.airbears2.1918.berkeley.edu>
2015-12-07 16:44:09 -0800

    Add documentation and tests to plot_tool()

--- code/tests/test_plot_tool.py
+++ code/tests/test_plot_tool.py
@@ -1,19 +1,39 @@
 """
-Testing plotting functions
+Testing plotting utilities contained in plot_tool module
+
+Tests can be run from project main directory with
+    nosetests code/tests/test_plot_tool
 """
 from __future__ import division, print_function, absolute_import
-from nose.tools import assert_equal
+from nose.tools import assert_raises
+from numpy.testing import assert_array_equal
 import numpy as np
 import sys
+
 sys.path.append("code/utils")
 from plot_tool import *
 
-def test_show3Dimage():
-    data3d = np.arange(512)
-    data3d.shape = (8,8,8)
-    data4d = np.arange(256)
-    data4d.shape = (4,4,4,4)
-    a = show3Dimage(data3d, time=None)
-    b = show3Dimage(data4d, time=2)
-    assert_equal(a.shape, (24, 24))
-    assert_equal(b.shape, (8, 8))
+def test_plot_volume():
+
+    # Import test data
+    data_file = "data/ds005/subtest/BOLD/task001_run001/bold.nii.gz"
+    data4d = nib.load(data_file).get_data()
+    data3d = data4d[..., 0]
+
+    # Tests argument volume functionality
+    assert_raises(AssertionError, plot_volume, data4d)
+
+    # Define test data sets
+    canvas1 = plot_volume(data3d)
+    canvas2 = plot_volume(data4d, 0)
+    canvas3 = plot_volume(data4d, 1)
+    canvas4 = plot_volume(data4d, 2)
+    canvases = [canvas1, canvas2, canvas3, canvas4]
+
+    # Check shapes and contents
+    for canvas in canvases: assert canvas.shape == (6, 6)
+    for canvas in canvases: assert_array_equal(canvas[3:, 3:], np.zeros((3, 3)))
+    assert_array_equal(canvas1, canvas2)
+    assert canvas1.sum() == data3d.sum()
+    assert canvas3.sum() - canvas2.sum() == 54
+    assert canvas4.sum() - canvas3.sum() == 54
--- code/utils/plot_tool.py
+++ code/utils/plot_tool.py
@@ -1,44 +1,50 @@
 """
-Plotting tools
---------------
+This script contains tools that will be used to plot findings from statistical
+analyses. Future Python scripts can take advantage of these utilities by
+including the command
+    sys.path.append("code/utils")
+    from plot_tool import *
 """
 from __future__ import division, print_function, absolute_import
 import matplotlib.pyplot as plt
 import nibabel as nib
 import numpy as np
     
-def show3Dimage(data, time=None):
+def plot_volume(data, volume=None):
     """
-    Plot all horizontal slices of 4D fMRI image at a given point in time.
+    Plots all horizontal slices of a fMRI volume.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     data : np.ndarray
-        4D array of fMRI data
-    time : int
-        The index (with respect to time) of the volume to plot
+        3- or 4-D array containing data imported from a .nii file
+    volume : int, optional
+        The index (with respect to time) of the volume of interest
 
-    Return:
+    Return
     -------
-    canvas: 2D array
-        Canvas of horizontal slices of the brain at a given time
+    canvas : 2-D array
+        Canvas depicting BOLD signal intensities of a given brain volume,
+        organized left-to-right and top-to-bottom respectively in grid format
     """
-    assert time == None or time <= data.shape[3]
-    if time == None:
-        length, width, depth = data.shape
-    else:
-        length, width, depth, timespan = data.shape
-    len_side = int(np.ceil(np.sqrt(depth))) # Number slices per side of canvas
-    canvas = np.zeros((length * len_side, width * len_side))
-    depth_i = 0 # The ith slice with respect to depth
-    for row in range(len_side):
+    # Check assertions
+    if data.ndim == 4:
+        assert volume != None and volume <= data.shape[3], "volume out of range"
+        data = data[..., volume]
+    elif data.ndim != 3:
+        assert False, "incorrect number of dimensions"
+    # Extract data to be used for plotting
+    length, width, depth = data.shape
+    # Canvas is a grid: compute the number of slices to plot per side
+    side_length = int(np.ceil(np.sqrt(depth)))
+    canvas = np.zeros((length * side_length, width * side_length))
+    # Plot slices iteratively: depth_i is the ith slice with respect to depth
+    depth_i = 0
+    for row in range(side_length):
         column = 0
-        while column < len_side and depth_i < depth:
-            if time == None:
-                canvas[length * row:length * (row + 1), width * column:width * (column + 1)] = data[:, :, depth_i]
-            else:
-                canvas[length * row:length * (row + 1), width * column:width * (column + 1)] = data[:, :, depth_i, time]
-            depth_i += 1
+        while column < side_length and depth_i < depth:
+            canvas[length * row:length * (row + 1),
+                   width * column:width * (column + 1)] = data[..., depth_i]
             column += 1
+            depth_i += 1
     return canvas
-
