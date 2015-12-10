b5d63c8ccdf460e0b63cc9ac172f14de5fec3d7c
Victor Kong <VictorKong@Victors-MacBook-Pro.local>
2015-12-10 00:29:59 -0800

    Oops Forgot to track the new one

--- code/scripts/smoothing.py
+++ code/scripts/smoothing.py
@@ -2,8 +2,13 @@
 Purpose
 -------
 This script applies smoothing with a Gaussian kernel in bulk to both the raw and
-the filtered BOLD signal data. The kernel used 
+the filtered BOLD signal data. The kernel used has a full-width-at-half-maximum
+measurement of 5 millimeters, which corresponds to a standard deviation of
+approximately 2.355 millimeters.
 
+It should output four files per run: two each for the raw and the filtered data,
+one of which is the image before smoothing and the other the image after
+smoothing.
 """
 from __future__ import absolute_import, division, print_function
 import matplotlib.pyplot as plt
@@ -14,23 +19,56 @@ sys.path.append("code/utils")
 from make_class import *
 from plot_tool import *
 
-image_location = 
 
-# Read in 4-dimensional filtered data
-sub = run('001', '001', filtered_data = True)
+# Create a collection of all subject IDs and all run IDs
+subject_IDs = [str(i).zfill(3) for i in range(1, 17)]
+run_IDs = [str(i).zfill(3) for i in range(1, 4)]
+IDs = list(zip([subject_ID for _ in range(3) for subject_ID in subject_IDs],
+              [run_ID for _ in range(16) for run_ID in run_IDs]))
 
-# Use our .smooth method from class 
-after_smooth = sub.smooth(sub.sigma_filtered)
 
-#align the two plots
-plt.imshow(show3Dimage(after_smooth, 50))
-plt.colorbar()
-plt.title('After Smoothed Data')
-plt.savefig(image_location+'Smoothed_data.png')
-plt.close()
+# We perform the procedure outlined below for each run of each subject:
+for ID in IDs:
+    subject, run = ID
 
-plt.imshow(show3Dimage(sub.data, 50))
-plt.colorbar()
-plt.title('Original Data')
-plt.savefig(image_location+'Original_data.png')
-plt.close()
+
+    # Extract the data of interest
+    obj = ds005(subject, run)
+
+
+    # Define results directories to which to save the figures produced
+    path_result = "results/sub%s_run%s/smoothing/" % (subject, run)
+    bash_command = "mkdir -p " + path_result
+    os.system(bash_command)
+
+
+    # Each figure will be plotted with the help of the plot_volume() utility
+    # contained in the plot_tool module. It produces a two-dimensional grid-like
+    # canvas on which each horizontal slice of the brain is shown as a tile.
+    raw_original = plot_volume(obj.raw.data, 50)
+    plt.imshow(raw_original)
+    plt.colorbar()
+    plt.title("Raw Data: Before Smoothing")
+    plt.savefig(path_results + "raw_original.png")
+    plt.close()
+
+    raw_smoothed = obj.raw.smooth()
+    plt.imshow(raw_smoothed)
+    plt.colorbar()
+    plt.title("Raw Data: After Smoothing")
+    plt.savefig(path_results + "raw_smoothed.png")
+    plt.close()
+
+    filtered_original = obj.filtered.data
+    plt.imshow(filtered_original)
+    plt.colorbar()
+    plt.title("Filtered Data: Before Smoothing")
+    plt.savefig(path_results + "filtered_original.png")
+    plt.close()
+
+    filtered_smoothed = obj.filtered.smooth()
+    plt.imshow(filtered_smoothed)
+    plt.colorbar()
+    plt.title("Filtered Data: After Smoothing")
+    plt.savefig(path_results + "filtered_smoothed.png")
+    plt.close()
