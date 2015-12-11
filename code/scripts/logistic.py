f090f6209d41526eeb5eb8c5bf9ad1eabc8f7154
Victor Kong <VictorKong@Victors-MacBook-Pro.local>
2015-12-11 04:47:45 -0800

    Add output to logistic.py

--- code/scripts/logistic.py
+++ code/scripts/logistic.py
@@ -18,6 +18,13 @@ from hypothesis import *
 from make_class import *
 
 
+# Create plaintext files in which to store Lambdas
+with open("results/lambda_euclidean_dist.txt", "w") as outfile:
+        outfile.write("run\tsubject\tlambda\n")
+with open("results/lambda_no_euclidean_dist.txt", "w") as outfile:
+        outfile.write("run\tsubject\tlambda\n")
+
+
 # Create a collection of all subject IDs and all run IDs
 run_IDs = [str(i).zfill(3) for i in range(1, 4)]
 subject_IDs = [str(i).zfill(3) for i in range(1, 17)]
@@ -30,6 +37,15 @@ for ID in IDs:
     run, subject = ID
 
 
+    # Create a directory to store the script's output
+    path_result = "results/run%s/logistic/sub%s/" % ID
+    try:
+        os.makedirs(path_result)
+    except OSError:
+        if not os.path.isdir(path_result):
+            raise
+
+
     # Import the necessary data:
     # - X will be our design matrix. By default, the .design_matrix() method
     #   produces a numpy array with exactly four columns and as many rows as
@@ -40,18 +56,31 @@ for ID in IDs:
     #   responses, 0 representing decline and 1 representing acceptance.
     obj = ds005(subject, run)
     X = obj.design_matrix()
-    responses = obj.behav[:, 5]
+    Y = obj.behav[:, 5]
 
 
     # Create an instance of LogisticRegression() and fit the data
-    log_model = LogisticRegression().fit(X, responses)
+    log_model = LogisticRegression().fit(X, Y)
 
 
     # Use the class to estimate our four coefficients
-    beta_hat = log_model.coef_.ravel()
-    print("The estimated coefficients are {}".format(beta_hat))
-    lamda = -beta_hat[2] / beta_hat[1]
-    print("The behavioral loss aversion score (lambda) is {}".format(lamda))
+    regr_coef = log_model.coef_.ravel()
+    regr_coef_tuple = tuple(value for value in regr_coef)
+    with open(path_result + "results.txt", "w") as outfile:
+        newline = ("euclidean_distance_included:\n\n" +
+                   "regr_coef:\n" +
+                   ("\tintercept:\t%s\n" % regr_coef_tuple[0]) +
+                   ("\tparam_gain:\t%s\n" % regr_coef_tuple[1]) +
+                   ("\tparam_loss:\t%s\n" % regr_coef_tuple[2]) +
+                   ("\teuclid_dist:\t%s\n\n" % regr_coef_tuple[3]))
+        outfile.write(newline)
+
+
+    # Compute Lambda and save its value to `lambda.txt`
+    Lambda = -regr_coef[2] / regr_coef[1]
+    with open("results/lambda_euclidean_dist.txt", "a") as outfile:
+        newline = (run + "\t" + subject + "\t" + str(Lambda) + "\n")
+        outfile.write(newline)
 
 
     # Next, we'll check our model's accuracy
@@ -59,15 +88,23 @@ for ID in IDs:
     probability_estimates = log_model.predict_proba(X)
     predictions = np.zeros(num_rows)
     predictions[probability_estimates[:, 1] > 0.5] = 1
-    misclassification_rate = np.sum(predictions != responses) / num_rows
-    print("The misclassification rate is {}".format(misclassification_rate))
+    misclassification_rate = np.sum(predictions != Y) / num_rows
+    with open(path_result + "results.txt", "a") as outfile:
+        newline = ("misclass_rate:\t%s\n\n" % misclassification_rate)
+        outfile.write(newline)
 
 
     # Lastly, we perform a Wald test to assess the statistical significance of
     # each of the three regressors
-    p_values = waldtest(X, beta_hat, probability_estimates)
-    print("The p values for each coeffiecient are {}".format(p_values))
-    print("=" * 80 + "\n")
+    p_values = waldtest(X, regr_coef, probability_estimates)
+    p_values_tuple = tuple(value for value in p_values)
+    with open(path_result + "results.txt", "a") as outfile:
+        newline = ("stat_signif:\n" +
+                   ("\tintercept:\t%s\n" % p_values_tuple[0]) +
+                   ("\tparam_gain:\t%s\n" % p_values_tuple[1]) +
+                   ("\tparam_loss:\t%s\n" % p_values_tuple[2]) +
+                   ("\teuclid_dist:\t%s\n\n\n" % p_values_tuple[3]))
+        outfile.write(newline)
 
 
     # Now, we'll perform one more analysis, without euclidean distance as a
@@ -78,26 +115,44 @@ for ID in IDs:
 
 
     # Create another fitted instance of LogisticRegression()
-    log_model2 = LogisticRegression().fit(X2, responses)
+    log_model2 = LogisticRegression().fit(X2, Y)
 
 
     # Estimate our three coefficients
-    beta_hat2 = log_model2.coef_.ravel()
-    print("The esitmated coeffiecients (without euclidean distance) are" +
-          " {}".format(beta_hat2))
-    lamda2 = -beta_hat2[2] / beta_hat2[1]
-    print("The Behavioral loss aversion (lambda) is {}".format(lamda2))
+    regr_coef2 = log_model2.coef_.ravel()
+    regr_coef2_tuple = tuple(value for value in regr_coef2)
+    with open(path_result + "results.txt", "a") as outfile:
+        newline = ("euclidean_distance_excluded:\n\n" +
+                   "regr_coef:\n" +
+                   ("\tintercept:\t%s\n" % regr_coef2_tuple[0]) +
+                   ("\tparam_gain:\t%s\n" % regr_coef2_tuple[1]) +
+                   ("\tparam_loss:\t%s\n\n" % regr_coef2_tuple[2]))
+        outfile.write(newline)
+
+
+    # Compute Lambda and save its value to `lambda.txt`
+    Lambda2 = -regr_coef2[2] / regr_coef2[1]
+    with open("results/lambda_no_euclidean_dist.txt", "a") as outfile:
+        newline = (run + "\t" + subject + "\t" + str(Lambda2) + "\n")
+        outfile.write(newline)
 
 
     # Calculate the new model's rate of misclassification
     probability_estimates2 = log_model2.predict_proba(X2)
     predictions2 = np.zeros(num_rows)
     predictions2[probability_estimates2[:, 1] > 0.5] = 1
-    misclassification_rate2 = np.sum(predictions2 != responses) / num_rows
-    print("The misclassification rate is {}".format(misclassification_rate2))
-
+    misclassification_rate2 = np.sum(predictions2 != Y) / num_rows
+    with open(path_result + "results.txt", "a") as outfile:
+        newline = ("misclass_rate:\t%s\n\n" % misclassification_rate2)
+        outfile.write(newline)
 
     # Another Wald test to assess the statistical significance of our two
     # regressors, without euclidean distance
-    p_values2 = waldtest(X2, beta_hat2, probability_estimates2)
-    print("The p values for each coeffiecient are {}".format(p_values2))
+    p_values2 = waldtest(X2, regr_coef2, probability_estimates2)
+    p_values2_tuple = tuple(value for value in p_values2)
+    with open(path_result + "results.txt", "a") as outfile:
+        newline = ("stat_signif:\n" +
+                   ("\tintercept:\t%s\n" % p_values2_tuple[0]) +
+                   ("\tparam_gain:\t%s\n" % p_values2_tuple[1]) +
+                   ("\tparam_loss:\t%s\n" % p_values2_tuple[2]))
+        outfile.write(newline)
