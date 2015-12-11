d110904b166a13ea391d8bcce141274a3b1f778f
Victor Kong <VictorKong@Victors-MacBook-Pro.local>
2015-12-11 03:24:57 -0800

    Add glm_util() to stat_utils

---
+++ code/tests/test_stat_utils.py
@@ -0,0 +1,47 @@
+"""
+Tests correlation module functionality
+
+Tests can be run from the project main directory with:
+    nosetests code/tests/test_stat_utils.py
+"""
+from __future__ import absolute_import, division, print_function
+from nose.tools import assert_almost_equal, assert_raises
+from numpy.testing import assert_array_equal
+import numpy as np
+import sys
+
+sys.path.append("code/utils")
+from stat_utils import *
+from make_class import *
+
+def test_correlation():
+
+    # Define an instance of class ds005
+    obj = ds005("test", "001")
+
+    # Test proper raising of AssertionErrors
+    assert_raises(AssertionError, correlation, obj.raw, "raw", "gain")
+    assert_raises(AssertionError, correlation, obj, "Raw", "gain")
+
+    # Test correlation output
+    corr = correlation(obj, "raw", "gain")
+    expected = np.ones((3, 3, 3))
+    assert_array_equal(corr, expected)
+
+def glm_util():
+
+    # Define an instance of class ds005
+    obj = ds005("test", "001")
+
+    # Create a valid design matrix (the generalize linear model acts strangely
+    # in the case of high dimensionality) and extract the response variable
+    design_matrix = obj.design_matrix(gain=False, loss=False,
+                                      euclidean_dist=False)
+    response = obj.behav[:, 5]
+
+    # Save and assess the output
+    regression_coefficients, df, MRSS = glm_util(design_matrix, response)
+    assert_almost_equal(regression_coefficients[0], 0.5)
+    assert df == 1
+    assert MRSS == 0.5
+    
--- code/utils/stat_utils.py
+++ code/utils/stat_utils.py
@@ -1,14 +1,15 @@
 """
-This script contains code that will assist in calculating the correlation
-coefficient for individual voxels across the dimension of time. Future Python
-scripts can take advantage of the correlation module by including the command
+This script contains code that performs a number of computations that tend to be
+very useful in statistical analysis. Future Python scripts can take advantage of
+this module by including the command
     sys.path.append("code/utils")
-    from correlation import *
+    from stat_utils import *
 """
 from __future__ import absolute_import, division, print_function
 import matplotlib.pyplot as plt
 import nibabel as nib
 import numpy as np
+import numpy.linalg as npl
 import os, sys
 from scipy.ndimage.filters import gaussian_filter
 
@@ -45,3 +46,36 @@ def correlation(obj, img, regressor):
     corr_1d = [np.corrcoef(voxel, time_course)[0, 1] for voxel in voxels]
     corr = np.reshape(corr_1d, data.shape[:3])
     return corr
+
+def glm_util(design_matrix, response):
+    """
+    Fits a generalized linear model to a set of training data.
+
+    Parameters
+    ----------
+    design_matrix : np.ndarray
+        2-D array with rows that correspond to observations and columns that
+        correspond to regressors. Let the shape of design_matrix be (N, P)
+    response : np.ndarray
+        1- or 2-D array representing the response variable. Let the shape of
+        response be (N, X)
+
+    Return
+    ------
+    regression_coefficients : np.ndarray
+        Array of shape (P, X) containing the coefficient of the regressor for
+        the individual data point
+    df : int
+        Degrees of freedom, which is the difference of the number of independent
+        regressors from the number of observations
+    MRSS : float
+        Mean residual sum of squares, a commmonly used measure of a predictive
+        model's accuracy (lower is better)
+    """
+    regression_coefficients = npl.pinv(design_matrix).dot(response)
+    prediction = design_matrix.dot(regression_coefficients)
+    error = response - prediction
+    RSS = (error ** 2).sum(0)
+    df = int(design_matrix.shape[0] - npl.matrix_rank(design_matrix))
+    MRSS = RSS / df
+    return (regression_coefficients, df, MRSS)
